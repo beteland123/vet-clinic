faf0e71d2bcec0782240a753b39cd24116b93b19
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-08-02 22:22:24 +0300

    add queries

--- queries.sql
+++ queries.sql
@@ -75,3 +75,58 @@ GROUP BY neutered;
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 /*What is the average number of escape attempts per animal type of those born between 1990 and 2000?*/
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-1-1' AND '1999-12-31' GROUP BY species;
+
+/*third day*/
+/*What animals belong to Melody Pond?*/
+SELECT name,date_of_birth
+FROM animals
+INNER JOIN owners
+ON animals.owner_id = owners.id
+WHERE owners.full_name ='Melody Pond';
+
+/*List of all animals that are pokemon (their type is Pokemon).*/
+
+SELECT animals.name
+FROM animals
+INNER JOIN species
+ON animals.species_id = species.id
+WHERE species.name = 'Pokemon';
+
+/*List all owners and their animals, remember to include those that don't own any animal.*/
+SELECT owners.full_name AS owner_name, animals.name AS animal_name
+FROM owners
+LEFT JOIN animals
+ON animals.owner_id = owners.id;
+
+/*
+How many animals are there per species?*/
+SELECT species.name,COUNT(animals.name) AS total_animal
+FROM animals
+INNER JOIN species
+ON animals.species_id = species.id
+GROUP BY species.name;
+/*List all Digimon owned by Jennifer Orwell.*/
+SELECT animals.name
+FROM animals
+INNER JOIN species
+ON animals.species_id = species.id
+INNER JOIN owners
+ON animals.owner_id = owners.id
+WHERE owners.full_name ='Jennifer Orwell' AND species.name = 'Digimon';
+
+/*List all animals owned by Dean Winchester that haven't tried to escape.*/
+SELECT animals.name As animals_name
+FROM animals
+INNER JOIN owners
+ON animals.owner_id = owners.id
+WHERE owners.full_name ='Dean Winchester'
+AND animals.escape_attempts = 0 ;
+
+/*Who owns the most animals?*/
+SELECT owners.full_name AS owner_name, COUNT(animals.id) AS animal_total
+FROM animals
+INNER JOIN owners
+ON animals.owner_id = owners.id
+GROUP BY owners.full_name
+ORDER BY animal_total DESC
+LIMIT 1;
