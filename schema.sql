b636d7b33862cd6e54d392b45ff5d53cf9ac9b65
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-08-02 22:23:35 +0300

    Add species schema and owner schema

--- schema.sql
+++ schema.sql
@@ -11,3 +11,18 @@ CREATE TABLE animals (
 /* the second day task*/
 /*add species*/
 ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+/* 3rd day*/
+ CREATE TABLE owners (
+    id SERIAL PRIMARY KEY NOT NULL,
+    full_name VARCHAR(100) NOT NULL,
+    age INT
+
+ );
+ CREATE TABLE species(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(100)
+ );
+ ALTER TABLE animals DROP COLUMN species;
+ ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
+ 
+ ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
