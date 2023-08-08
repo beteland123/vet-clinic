3d6c797ce5c803f9c1709f72d0f05a486b5bc5ab
JuanDBta <juandavid_diaz@hotmail.com>
2023-08-08 14:56:53 -0500

    Create treatments and treatments_card tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,16 @@ CREATE TABLE medical_histories(
     patient_id INT REFERENCES patients(id),
     status VARCHAR(50)
 );
+
+CREATE TABLE treatment_card(
+    med_id INTEGER REFERENCES  medical_histories(id) NOT NULL,
+    treatments_id INTEGER REFERENCES treatments(id) NOT NULL,
+    PRIMARY KEY (med_id, treatments_id)
+
+); 
+
+CREATE TABLE treatments (
+    id SERIAL PRIMARY KEY NOT NULL,
+    type VARCHAR(50),
+    NAME VARCHAT(50)
+);
