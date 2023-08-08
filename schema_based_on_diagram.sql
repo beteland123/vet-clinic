bfcb2a6ca303892c62ed200b680743e872835f34
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-08-08 23:05:49 +0300

    Add the invoice_items table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -7,8 +7,8 @@ CREATE TABLE medical_histories(
 
 CREATE TABLE treatment_card(
     med_id INTEGER REFERENCES  medical_histories(id) NOT NULL,
-    treatments_id INTEGER REFERENCES treatments(id) NOT NULL,
-    PRIMARY KEY (med_id, treatments_id)
+    treatment_id INTEGER REFERENCES treatments(id) NOT NULL,
+    PRIMARY KEY (med_id, treatment_id)
 
 ); 
 
@@ -16,4 +16,12 @@ CREATE TABLE treatments (
     id SERIAL PRIMARY KEY NOT NULL,
     type VARCHAR(50),
     NAME VARCHAT(50)
+);
+CREATE TABLE invoice_items(
+   id SERIAL PRIMARY KEY NOT NULL,
+   unit_price DECIMAL(10,2),
+   quantity INT,
+   total_price DECIMAL(10,2),
+   invoice_id INTEGER REFERENCES invoices(id),
+   treatment_id INTEGER REFERENCES treatments(id),
 );
